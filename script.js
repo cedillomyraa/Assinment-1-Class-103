83e881e94d928d34e2a126d8b8d40971854f9e19
cedillomyraa <97204012+cedillomyraa@users.noreply.github.com>
2022-01-29 09:22:09 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+let firstName = "Yesenia";
+let lastName = "Rodriguez"
+let age = 25
+let height = "5ft"
+let dob = "Jan-04-1997"
+let pob = "Dallas TX"
+let nationality = "Mexican"
+let languages = "Spanish and English"
+let hobbies = "Reading, Painting, and Cooking"
+let favriotmovies = "Man on Fire, Sing, and August Rush"
+let favriotshows = "Criminal Minds, Scandal, and Dexter"
+let favriotbooks = "Start Whith Why and 40 Rules of Love"
+let currentcity = "Denver, CO"
+let profession = "Teacher"
+let favriotfood = "Sushi and Tacos"
+let maritalstatus = "Single"
+let family = "Family of five, Mother, two brother, and one sister"
+let pets= "N/A"
+let favriotcolor= "Red"
+let dreamplace= "Spain"
+
+
+console.log("First Name:",firstName );
+console.log("Last Name:",lastName );
+console.log("Age: ",age );
+console.log("Height: ",height );
+console.log("DOB: ",dob );
+console.log("POB: ",pob );
+console.log("Nationality: ",nationality );
+console.log("Languages Spoken: ",languages );
+console.log("Hobbies: ",hobbies );
+console.log("Favriot Movies: ",favriotmovies );
+console.log("Favriot Shows: ",favriotshows );
+console.log("Favriot Books: ",favriotbooks );
+console.log("Current City: ",currentcity );
+console.log("Profession: ",profession );
+console.log("Foods: ",favriotfood );
+console.log("Marital Status: ",maritalstatus );
+console.log("Family Members: ",family );
+console.log("Pets: ",pets );
+console.log("Favriot Color: ",favriotcolor );
+console.log("Dream Place to Live: ",dreamplace );
